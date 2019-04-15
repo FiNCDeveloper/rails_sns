9ebb77ab382e3d642ab0e2099e32645745f70d4f
ryok-0319 <ryo.kubota.i@finc.com>
2019-04-15 16:51:28 +0900

    fix initializer

--- config/initializers/clientoken.rb
+++ config/initializers/clientoken.rb
@@ -1,5 +1,6 @@
 if Rails.env.development?
   Clientoken.setup do |config|
+    config.clientoken_env = ENV.fetch('CLIENTOKEN_ENV')
     config.administoken_master_token = 'aeac9179ad953090d2d98b61a524e9fc'
   end
 end

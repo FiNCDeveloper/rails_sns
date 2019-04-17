6a32bcbbb4dea06f8c9f548fc6ca1df2ddaf8104
ryok-0319 <ryo.kubota.i@finc.com>
2019-04-17 18:05:36 +0900

    delete initializer

--- config/initializers/clientoken.rb
+++ config/initializers/clientoken.rb
@@ -1,4 +1,3 @@
 Clientoken.setup do |config|
-  config.clientoken_env = ENV.fetch('CLIENTOKEN_ENV')
   config.administoken_master_token = 'aeac9179ad953090d2d98b61a524e9fc' if Rails.env.development?
 end

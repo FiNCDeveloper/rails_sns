d9004d68edc14b39dd2350a9e52ee2cc90d79bcf
ryok-0319 <ryo.kubota.i@finc.com>
2019-04-19 12:56:47 +0900

    migration

---
+++ db/migrate/20190419035630_create_clientoken_unauthorized_services.clientoken.rb
@@ -0,0 +1,14 @@
+# This migration comes from clientoken (originally 20190418052745)
+class CreateClientokenUnauthorizedServices < ActiveRecord::Migration[5.2]
+  def change
+    create_table :clientoken_unauthorized_services do |t|
+      t.string :application_name,  :null => false
+      t.datetime :invalid_until,   :null => false
+      t.string :application_token, :null => false
+
+      t.timestamps
+    end
+    add_index :clientoken_authorized_services, :application_name
+    add_index :clientoken_authorized_services, :application_token
+  end
+end
